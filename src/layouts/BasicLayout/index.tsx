d266b4516a04dc4b76e4dbcf9f31ae764fd191ee
77900 <779007247@qq.com>
2025-02-17 14:29:28 +0800

    前端初始代码

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,10 +1,10 @@
-"use client";
-import { GithubFilled, LogoutOutlined, UserOutlined } from "@ant-design/icons";
-import { ProLayout } from "@ant-design/pro-components";
-import { Dropdown, message } from "antd";
 import React from "react";
-import Image from "next/image";
+import { ConfigProvider } from "antd";
+import { ProLayout } from "@ant-design/pro-components";
+import { GithubFilled, LogoutOutlined, UserOutlined } from "@ant-design/icons";
+import { Dropdown, message } from "antd";
 import { usePathname, useRouter } from "next/navigation";
+import Image from "next/image";
 import Link from "next/link";
 import GlobalFooter from "@/components/GlobalFooter";
 import { menus } from "../../../config/menu";
@@ -18,142 +18,144 @@ import SearchInput from "@/layouts/BasicLayout/components/SearchInput";
 import "./index.css";
 
 interface Props {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
-/**
- * 全局通用布局
- * @param children
- * @constructor
- */
 export default function BasicLayout({ children }: Props) {
-  const pathname = usePathname();
-  // 当前登录用户
-  const loginUser = useSelector((state: RootState) => state.loginUser);
-  const dispatch = useDispatch<AppDispatch>();
-  const router = useRouter();
+    const pathname = usePathname();
+    const loginUser = useSelector((state: RootState) => state.loginUser);
+    const dispatch = useDispatch<AppDispatch>();
+    const router = useRouter();
 
-  /**
-   * 用户注销
-   */
-  const userLogout = async () => {
-    try {
-      await userLogoutUsingPost();
-      message.success("已退出登录");
-      dispatch(setLoginUser(DEFAULT_USER));
-      router.push("/user/login");
-    } catch (e) {
-      message.error("操作失败，" + e.message);
-    }
-  };
-
-  return (
-    <div
-      id="basicLayout"
-      style={{
-        height: "100vh",
-        overflow: "auto",
-      }}
-    >
-      <ProLayout
-        title="智刷宝"
-        layout="top"
-        logo={
-          <Image
-            src="/assets/logo.png"
-            height={32}
-            width={32}
-            alt="智刷宝 - HLM"
-          />
+    const userLogout = async () => {
+        try {
+            await userLogoutUsingPost();
+            message.success("已退出登录");
+            dispatch(setLoginUser(DEFAULT_USER));
+            router.push("/user/login");
+        } catch (e) {
+            message.error("操作失败，" + e.message);
         }
-        location={{
-          pathname,
-        }}
-        avatarProps={{
-          src: loginUser.userAvatar || "/assets/logo.png",
-          size: "small",
-          title: loginUser.userName || "用户",
-          render: (props, dom) => {
-            if (!loginUser.id) {
-              return (
-                <div
-                  onClick={() => {
-                    router.push("/user/login");
-                  }}
-                >
-                  {dom}
-                </div>
-              );
-            }
-            return (
-              <Dropdown
-                menu={{
-                  items: [
-                    {
-                      key: "userCenter",
-                      icon: <UserOutlined />,
-                      label: "个人中心",
-                    },
-                    {
-                      key: "logout",
-                      icon: <LogoutOutlined />,
-                      label: "退出登录",
-                    },
-                  ],
-                  onClick: async (event: { key: React.Key }) => {
-                    const { key } = event;
-                    if (key === "logout") {
-                      userLogout();
-                    } else if (key === "userCenter") {
-                      router.push("/user/center");
-                    }
-                  },
+    };
+
+    return (
+        <ConfigProvider
+            theme={{
+                // 设置主题颜色为蓝色
+                token: {
+                    colorPrimary: "#1890ff", // 蓝色
+                    colorBgBase: "#ffffff", // 白色背景
+                    colorBgContainer: "#f0f2f5", // 内容区背景
+                    colorTextBase: "#333", // 主文本颜色
+                    colorLink: "#1890ff", // 链接颜色
+                },
+            }}
+        >
+            <div
+                id="basicLayout"
+                style={{
+                    height: "100vh",
+                    overflow: "auto",
+                    backgroundColor: "#f0f2f5", // 设置整体背景色为浅白色
                 }}
-              >
-                {dom}
-              </Dropdown>
-            );
-          },
-        }}
-        actionsRender={(props) => {
-          if (props.isMobile) return [];
-          return [
-            <SearchInput key="search" />,
-            <a
-              key="github"
-              href="https://github.com/wahahahhaha/zhishuabao-project"
-              target="_blank"
             >
-              <GithubFilled key="GithubFilled" />
-            </a>,
-          ];
-        }}
-        headerTitleRender={(logo, title, _) => {
-          return (
-            <a>
-              {logo}
-              {title}
-            </a>
-          );
-        }}
-        // 渲染底部栏
-        footerRender={() => {
-          return <GlobalFooter />;
-        }}
-        onMenuHeaderClick={(e) => console.log(e)}
-        // 定义有哪些菜单
-        menuDataRender={() => {
-          return getAccessibleMenus(loginUser, menus);
-        }}
-        // 定义了菜单项如何渲染
-        menuItemRender={(item, dom) => (
-          <Link href={item.path || "/"} target={item.target}>
-            {dom}
-          </Link>
-        )}
-      >
-        {children}
-      </ProLayout>
-    </div>
-  );
+                <ProLayout
+                    title="智刷宝"
+                    layout="top"
+                    logo={
+                        <Image
+                            src="/assets/logo.png"
+                            height={32}
+                            width={32}
+                            alt="智刷宝 - HLM"
+                        />
+                    }
+                    location={{
+                        pathname,
+                    }}
+                    avatarProps={{
+                        src: loginUser.userAvatar || "/assets/logo.png",
+                        size: "small",
+                        title: loginUser.userName || "用户",
+                        render: (props, dom) => {
+                            if (!loginUser.id) {
+                                return (
+                                    <div
+                                        onClick={() => {
+                                            router.push("/user/login");
+                                        }}
+                                    >
+                                        {dom}
+                                    </div>
+                                );
+                            }
+                            return (
+                                <Dropdown
+                                    menu={{
+                                        items: [
+                                            {
+                                                key: "userCenter",
+                                                icon: <UserOutlined />,
+                                                label: "个人中心",
+                                            },
+                                            {
+                                                key: "logout",
+                                                icon: <LogoutOutlined />,
+                                                label: "退出登录",
+                                            },
+                                        ],
+                                        onClick: async (event: { key: React.Key }) => {
+                                            const { key } = event;
+                                            if (key === "logout") {
+                                                userLogout();
+                                            } else if (key === "userCenter") {
+                                                router.push("/user/center");
+                                            }
+                                        },
+                                    }}
+                                >
+                                    {dom}
+                                </Dropdown>
+                            );
+                        },
+                    }}
+                    actionsRender={(props) => {
+                        if (props.isMobile) return [];
+                        return [
+                            <SearchInput key="search" />,
+                            <a
+                                key="github"
+                                href="https://github.com/wahahahhaha/zhishuabao-project"
+                                target="_blank"
+                            >
+                                <GithubFilled key="GithubFilled" />
+                            </a>,
+                        ];
+                    }}
+                    headerTitleRender={(logo, title, _) => {
+                        return (
+                            <a>
+                                {logo}
+                                {title}
+                            </a>
+                        );
+                    }}
+                    footerRender={() => {
+                        return <GlobalFooter />;
+                    }}
+                    onMenuHeaderClick={(e) => console.log(e)}
+                    menuDataRender={() => {
+                        return getAccessibleMenus(loginUser, menus);
+                    }}
+                    menuItemRender={(item, dom) => (
+                        <Link href={item.path || "/"} target={item.target}>
+                            {dom}
+                        </Link>
+                    )}
+                >
+                    {children}
+                </ProLayout>
+            </div>
+        </ConfigProvider>
+    );
 }
